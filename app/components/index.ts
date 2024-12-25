fc22906dae7e823e20e6dd7ece88da0484eabe9f
BuciMr <51615775+baninations@users.noreply.github.com>
2024-12-25 23:37:02 +0100

    Refactor: Organize imports

---
+++ app/components/index.ts
@@ -0,0 +1,10 @@
+import Link from "./Link";
+import ErrorMessage from "./ErrorMessage";
+import IssueStatusBadge from "./IssueStatusBadge";
+import Spinner from "./Spinner";
+
+export { Link };
+export { ErrorMessage };
+export { IssueStatusBadge };
+export { Spinner };
+export { default as Skeleton } from "./Skeleton";
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,6 +1,5 @@
-import { Box, Flex, Card } from "@radix-ui/themes";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
+import { Box } from "@radix-ui/themes";
 
 export default function IssueNewLoading() {
   return (
