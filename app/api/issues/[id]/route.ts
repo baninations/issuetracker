22167a157f8bbc9f3998246acc7a5eb8c81d5add
BuciMr <51615775+baninations@users.noreply.github.com>
2025-01-02 00:46:36 +0100

    Build an API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -36,3 +36,26 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue, { status: 200 });
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!issue) return NextResponse.json("No Issue found", { status: 404 });
+
+  await prisma.issue.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  return NextResponse.json("Issue Deleted");
+}
